83022905931bf1d470ea95cd8a19c33fd69f1fb0
davisjoe <joseph@jjdavis.co.uk>
2017-06-20 20:06:59 +0000

    Motor and test timing code added

--- joes robot challenge.py
+++ joes robot challenge.py
@@ -1,9 +1,18 @@
-#pygame demo code 
+#Joe's Robot Challenge
 import pygame, sys
 from pygame.locals import *
 
 import usb.core
+import RPi.GPIO as GPIO
+import time
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(7, GPIO.OUT)
+GPIO.setup(8, GPIO.OUT)
+GPIO.setup(9, GPIO.OUT)
+GPIO.setup(10, GPIO.OUT)
 
 pygame.init()
 
@@ -22,6 +31,8 @@ x = 10
 y = 10
 speed = 50
 direction = 'right'
+puckWidth = 100
+puckHeight = 100
 
 
 dev = usb.core.find(idVendor=0x1130, idProduct=0x0202)
@@ -51,7 +62,7 @@ while not pressed: # the left-right loop
 
 
     #DISPLAYSURF.blit(catImg, (catx, caty))
-    pygame.draw.rect(DISPLAYSURF, (255,0,0), (x, y, 100, 50))
+    pygame.draw.rect(DISPLAYSURF, (255,0,0), (x, y, puckWidth, puckHeight))
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -63,3 +74,68 @@ while not pressed: # the left-right loop
     if dev.ctrl_transfer(bmRequestType=0xA1, bRequest=1, wValue=0x300, data_or_wLength=8, timeout=500)[0] :
       print ("Pressed at x = ", x)
       pressed = True
+
+#clear motor signals
+GPIO.output(7, 0)
+GPIO.output(8, 0)
+GPIO.output(9, 0)
+GPIO.output(10, 0)
+
+
+x = 10
+y = 10
+speed = 50
+direction = 'down'
+puckWidth = 100
+puckHeight = 100      
+pressed = False
+
+#forward (right wheel)
+#GPIO.output(9, 0)
+#GPIO.output(10, 1)
+
+#forward (left wheel)
+GPIO.output(7, 0)
+GPIO.output(8, 1)
+
+initialT = pygame.time.get_ticks()
+delayMS = 1000
+while not pressed: # the up-down loop
+    DISPLAYSURF.fill(SNOW)
+
+    if direction == 'down':
+        y += speed
+        if y >= height:
+            direction = 'up'
+
+    elif direction == 'up':
+        y -= speed
+        if y <= 10:
+            direction = 'down'
+
+
+    #DISPLAYSURF.blit(catImg, (catx, caty))
+    pygame.draw.rect(DISPLAYSURF, (255,0,0), (x, y, puckWidth, puckHeight))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    fpsClock.tick(FPS)
+    if dev.ctrl_transfer(bmRequestType=0xA1, bRequest=1, wValue=0x300, data_or_wLength=8, timeout=500)[0] :
+      print ("Pressed at y = ", y)
+      pressed = True
+    if (initialT + delayMS) < pygame.time.get_ticks() :
+        print("time elapsed")
+        pressed = True
+            
+#clear motor signals
+GPIO.output(7, 0)
+GPIO.output(8, 0)
+GPIO.output(9, 0)
+GPIO.output(10, 0)
+GPIO.cleanup()
+
+pygame.quit()
